e44a14200f047825761da80a002a3d32b6dff800
Kurt Galiatsatos <soad1789@gmail.com>
2017-07-17 16:11:33 +0000

    Checkpoint

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", function (req, res) { // Serve the homepage
+  var fileName = path.join("views", "index.html");
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    } else {
+      console.log("View sent:", fileName)
+    }
+  });
+});
+
+app.get("/api/whoami", function (res, re))
