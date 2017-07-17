e951b4b10b195b45aac9ab06ffdf4de493ee20ed
Kurt Galiatsatos <soad1789@gmail.com>
2017-07-17 19:02:14 +0000

    Build simple Express server with static route and addtional route which returns basic HTTP req headers in JSON format

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ app.get("/", function (req, res) { // Serve the homepage
 
 app.get("/api/whoami", function (req, res) {
   var filterLog = /[\:{2}\w{4}\:]*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\,/;
-  var ip = filterLog.exec(req.headers['x-forwarded-for'])[0].;
+  var ip = filterLog.exec(req.headers['x-forwarded-for'])[0];
+  ip = ip.substr(0, ip.length - 1);
+  
   var response = {
     ipaddress: ip,
     language: req.get("Accept-Language"),
     software: req.get("User-Agent")
   };
+  
   res.json(response);
 });
 
